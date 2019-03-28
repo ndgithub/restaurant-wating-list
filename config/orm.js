57b135fe9077802043621e55f433bc3f5a2fc5b3
ndgithub <nickydesai.mail@gmail.com>
2019-03-27 21:28:49 -0500

    Orm and customer model are working

--- config/orm.js
+++ config/orm.js
@@ -2,30 +2,29 @@ var connection = require('./connection.js')
 
 
 var orm = {
-  selectAll: function (callback) {
-    var queryString = "SELECT * FROM customers";
+  selectAll: function (table, callback) {
+    var queryString = `SELECT * FROM ${table}`;
     connection.query(queryString, function (err, result) {
       if (err) throw err;
-      console.log(result);
+
       callback(result);
     });
 
 
   },
-  insertOne: function (name, size, callback) {
+  insertOne: function (table, columnsArray, valuesArray, callback) {
     var checkInTime = (new Date).getTime();
-    var queryString = `INSERT INTO customers (name, size, check_in_time, seated) VALUES ('${name}', '${size}', ${checkInTime}, false)`;
-    connection.query(queryString, function (err, result) {
+    var queryString = `INSERT INTO ${table} (??) VALUES (?)`;
+    connection.query(queryString, [columnsArray, valuesArray], function (err, result) {
+      console.log(err);
       if (err) throw err;
-      console.log(result);
       callback(result)
     });
   },
-  updateOne: function (id, callback) {
-    var queryString = `UPDATE customers SET seated = 'true' WHERE id = ${id}`;
+  updateOne: function (table, colToUpdate, newColValue, id, callback) {
+    var queryString = `UPDATE ${table} SET ${colToUpdate} = '${newColValue}' WHERE id = '${id}'`;
     connection.query(queryString, function (err, result) {
       if (err) throw err;
-      console.log(result);
       callback(result)
     });
   }
--- models/customers.js
+++ models/customers.js
@@ -1,21 +1,32 @@
 var orm = require('../config/orm.js');
 
-
 var customer = {
   all: function (cb) {
-    orm.selectAll(function (res) {
+    orm.selectAll('customers', function (res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function (name, size, cb) {
-    orm.insertOne(name, size, function (res) {
+  create: function (name, size, time, cb) {
+    orm.insertOne('customers', ['name', 'size', 'check_in_time'], [name, size, time], function (res) {
       cb(res);
     });
   },
-  update: function (objColVals, condition, cb) {
-    orm.updateOne(id, function (res) {
+  updateSeated: function (id, cb) {
+    orm.updateOne('customers', 'seated', '1', id, function (res) {
       cb(res);
     });
   }
 };
+
+customer.create('stevie', '3', '1234', function (res) {
+  console.log(res);
+});
+
+customer.all(function (res) {
+  console.log(res);
+})
+
+customer.updateSeated(4, function (res) {
+  console.log(res);
+})
